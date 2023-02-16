200520758722c44ea65ab4c0020253f84f157151
beyangu <beyangu>
2023-02-16 23:05:47 +0300

    tt

--- basic_threads/primes/task.cpp
+++ basic_threads/primes/task.cpp
@@ -1,79 +1,93 @@
-//
-// Created by Татьяна Мельник on 16.02.2023.
-//
-#include "task.h"
-#include <mutex>
-#include <vector>
-PrimeNumbersSet::PrimeNumbersSet() = default;
-// Проверка, что данное число присутствует в множестве простых чисел
-bool PrimeNumbersSet::IsPrime(uint64_t number) const{
-    return primes_.find(number) != primes_.end();
-}
 
-// Получить следующее по величине простое число из множества
-uint64_t PrimeNumbersSet::GetNextPrime(uint64_t number) const{
-    auto it = primes_.upper_bound(number);
-    if( it != primes_.end()){
-        return *it;
-    }else{
-        throw std::invalid_argument("error");
-    }
-}
+
+#include <cstdint>
+#include <mutex>
+#include <set>
+#include <atomic>
+#include <vector>
 
 /*
- * Найти простые числа в диапазоне [from, to) и добавить в множество
- * Во время работы этой функции нужно вести учет времени, затраченного на ожидание лока мюьтекса,
- * а также времени, проведенного в секции кода под локом
+ * Класс PrimeNumbersSet -- множество простых чисел в каком-то диапазоне
  */
-void PrimeNumbersSet::AddPrimesInRange(uint64_t from, uint64_t to){
-    const std::chrono::time_point start1 = std::chrono::high_resolution_clock::now();////maybe steady_clock???
-    std::unique_lock<std::mutex> lo{set_mutex_, std::defer_lock};
-    lo.lock();
-    const std::chrono::time_point start = std::chrono::high_resolution_clock::now();
+class PrimeNumbersSet {
+public:
+    PrimeNumbersSet() = default;
 
-    std::vector<bool> pr(to, true);
-    pr[0] = pr[1] = false;
-    for(uint64_t i = 2; i < to; ++i){
-        if(pr[i]){
-            if(i >= from)
-                primes_.insert(i);
-            for(uint64_t j = i * i; j < to; j += i){
-                pr[j] = false;
-            }
+    // Проверка, что данное число присутствует в множестве простых чисел
+    bool IsPrime(uint64_t number) const{
+        std::lock_guard<std::mutex> lo(set_mutex_);
+        return primes_.find(number) != primes_.end();
+    }
+
+    // Получить следующее по величине простое число из множества
+    uint64_t GetNextPrime(uint64_t number) const{
+        std::lock_guard<std::mutex> lo(set_mutex_);
+        auto it = primes_.upper_bound(number);
+        if( it != primes_.end()){
+            return *it;
+        }else{
+            throw std::invalid_argument("error");
         }
     }
 
+    void AddPrimesInRange(uint64_t from, uint64_t to){
 
-    const std::chrono::time_point finish = std::chrono::high_resolution_clock::now();
-    nanoseconds_under_mutex_.fetch_add(std::chrono::duration<uint64_t, std::nano>(finish - start).count());
-    nanoseconds_waiting_mutex_.fetch_add(std::chrono::duration<uint64_t, std::nano>(finish - start1).count());
-    lo.unlock();
-}
 
-// Посчитать количество простых чисел в диапазоне [from, to)
-size_t PrimeNumbersSet::GetPrimesCountInRange(uint64_t from, uint64_t to) const{
-    auto itStart = primes_.lower_bound(from);
-    auto itEnd = primes_.lower_bound(to);
-    size_t ans = 0;
-    for(auto it = itStart; it != itEnd; ++it){
-        ++ans;
+        for(uint64_t i = from; i < to; ++i){
+            bool fl = true;
+            for(uint64_t j = 2; j * j <= i; ++j){
+                if(i%j == 0){
+                    fl = false;
+                    break;
+                }
+            }
+            if(fl && i != 1 && i != 0){
+                const std::chrono::time_point start = std::chrono::high_resolution_clock::now();
+                set_mutex_.lock();
+                const std::chrono::time_point finish = std::chrono::high_resolution_clock::now();
+                nanoseconds_waiting_mutex_.fetch_add(std::chrono::duration<uint64_t, std::nano>(finish - start).count());
+                primes_.insert(i);
+                set_mutex_.unlock();
+                const std::chrono::time_point finish2 = std::chrono::high_resolution_clock::now();
+                nanoseconds_under_mutex_.fetch_add(std::chrono::duration<uint64_t, std::nano>(finish2 - finish).count());
+
+            }
+        }
+
+
     }
-    if(itEnd != primes_.end() && *itEnd == to){ --ans; }
-    return ans;
-}
 
-// Получить наибольшее простое число из множества
-uint64_t PrimeNumbersSet::GetMaxPrimeNumber() const{
-    return *primes_.rbegin();
-}
+    // Посчитать количество простых чисел в диапазоне [from, to)
+    size_t GetPrimesCountInRange(uint64_t from, uint64_t to) const{
+        std::lock_guard<std::mutex> lo(set_mutex_);
+        auto itStart = primes_.lower_bound(from);
+        auto itEnd = primes_.lower_bound(to);
+        size_t ans = 0;
+        for(auto it = itStart; it != itEnd; ++it){
+            ++ans;
+        }
+        if(itEnd != primes_.end() && *itEnd == to){ --ans; }
+        return ans;
+    }
 
-// Получить суммарное время, проведенное в ожидании лока мьютекса во время работы функции AddPrimesInRange
-std::chrono::nanoseconds PrimeNumbersSet::GetTotalTimeWaitingForMutex() const{
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::nanoseconds(nanoseconds_waiting_mutex_));
-}
+    // Получить наибольшее простое число из множества
+    uint64_t GetMaxPrimeNumber() const{
+        std::lock_guard<std::mutex> lo(set_mutex_);
+        return *primes_.rbegin();
+    }
 
-// Получить суммарное время, проведенное в коде под локом во время работы функции AddPrimesInRange
-std::chrono::nanoseconds PrimeNumbersSet::GetTotalTimeUnderMutex() const{
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::nanoseconds((nanoseconds_under_mutex_)));
-    //return std::chrono::nanoseconds((nanoseconds_waiting_mutex_));
-}
+    // Получить суммарное время, проведенное в ожидании лока мьютекса во время работы функции AddPrimesInRange
+    std::chrono::nanoseconds GetTotalTimeWaitingForMutex() const{
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::nanoseconds(nanoseconds_waiting_mutex_));
+    }
+
+    // Получить суммарное время, проведенное в коде под локом во время работы функции AddPrimesInRange
+    std::chrono::nanoseconds GetTotalTimeUnderMutex() const{
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::nanoseconds((nanoseconds_under_mutex_)));
+        //return std::chrono::nanoseconds((nanoseconds_waiting_mutex_));
+    }
+private:
+    std::set<uint64_t> primes_;
+    mutable std::mutex set_mutex_;
+    std::atomic<uint64_t> nanoseconds_under_mutex_, nanoseconds_waiting_mutex_;
+};
