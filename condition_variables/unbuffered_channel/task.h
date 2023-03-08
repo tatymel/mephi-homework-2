40425610c8d4ae6d1333b4de0c75ee0680e0e970
beyangu <beyangu>
2023-03-08 17:49:02 +0300

    t1

--- condition_variables/unbuffered_channel/task.h
+++ condition_variables/unbuffered_channel/task.h
@@ -14,8 +14,47 @@ template<typename T>
 class UnbufferedChannel {
 public:
     void Put(const T& data) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        while(hasNotRead_){
+            canPut_.wait(lock);
+        }
+
+        value_ = data;
+        hasNotRead_ = true;
+        canGet_.notify_one();
     }
 
     T Get(std::chrono::milliseconds timeout = std::chrono::milliseconds(0)) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
+        while(!hasNotRead_){
+            canGet_.wait(lock/*, [&]{
+                std::chrono::time_point<std::chrono::steady_clock> finish = std::chrono::steady_clock::now();
+                if(finish - start > timeout){
+                    throw TimeOut();////
+                }
+                return;
+            }*/);
+        }
+
+         std::chrono::time_point<std::chrono::steady_clock> finish = std::chrono::steady_clock::now();
+        if(finish - start > timeout){
+            throw TimeOut();////
+        }
+
+        T val = value_;
+        hasNotRead_ = false;
+        canPut_.notify_one();
+        return val;
+
+
+
     }
+private:
+    T value_;
+    bool hasNotRead_ = false;
+    std::mutex mutex_;
+    std::condition_variable canPut_;
+    std::condition_variable canGet_;
+
 };
