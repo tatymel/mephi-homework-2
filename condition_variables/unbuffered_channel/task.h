08c8c5a24c14fc6c95df7972783546cc574c3042
beyangu <beyangu>
2023-03-16 12:42:41 +0300

    t1

--- condition_variables/unbuffered_channel/task.h
+++ condition_variables/unbuffered_channel/task.h
@@ -1,6 +1,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <optional>
+#include <atomic>
+#include <iostream>
 
 class TimeOut : public std::exception {
     const char* what() const noexcept override {
@@ -14,13 +16,19 @@ class UnbufferedChannel {
 public:
     void Put(const T& data) {
         std::unique_lock<std::mutex> lock(mutex_);
-        while(hasNotRead_){
+        while(!hasReadElemnt_){
             canPut_.wait(lock);
         }
 
         value_ = data;
-        hasNotRead_ = true;
+        hasReadElemnt_ = false;
         canGet_.notify_one();
+
+        otherThreadRead_ = false;
+
+        while (!otherThreadRead_){
+            blockThread_.wait(lock);
+        }
     }
 
     T Get(std::chrono::milliseconds timeout = std::chrono::milliseconds(0)) {
@@ -28,42 +36,49 @@ public:
         std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
 
         if(timeout == std::chrono::milliseconds(0)){
-            while (!hasNotRead_) {
+
+            while(hasReadElemnt_){
                 canGet_.wait(lock);
             }
 
             T val = value_;
-            hasNotRead_ = false;
+            hasReadElemnt_ = true;
+
+            otherThreadRead_ = true;
+            blockThread_.notify_all();
             canPut_.notify_one();
             return val;
-        }else {
-            while (!hasNotRead_) {
-                std::chrono::time_point<std::chrono::steady_clock> finish = std::chrono::steady_clock::now();
-                if(finish - start > timeout) {
-                    hasNotRead_ = true;
+        }else{
+            while(hasReadElemnt_){
+                if(canGet_.wait_until(lock, start + timeout) == std::cv_status::timeout){
+                    break;
                 }
-                canGet_.wait_until(lock, start + timeout);
             }
+            if(hasReadElemnt_){
+                otherThreadRead_ = true;
+                blockThread_.notify_all();
+                canPut_.notify_one();
 
-            std::chrono::time_point<std::chrono::steady_clock> finish = std::chrono::steady_clock::now();
-
-            if(finish - start > timeout) {
-                hasNotRead_ = false;
                 throw TimeOut();
             }else{
                 T val = value_;
-                hasNotRead_ = false;
+                hasReadElemnt_ = true;
+
+                otherThreadRead_ = true;
+                blockThread_.notify_all();
                 canPut_.notify_one();
                 return val;
             }
         }
 
+
     }
 private:
     T value_;
-    bool hasNotRead_ = false;
+    bool hasReadElemnt_ = true;
+    bool otherThreadRead_ = false;
     std::mutex mutex_;
     std::condition_variable canPut_;
     std::condition_variable canGet_;
-
+    std::condition_variable blockThread_;
 };
