302433ca56b218d0f85627ef2318ce6f03b43c96
beyangu <beyangu>
2023-03-23 13:31:06 +0300

    t1

--- atomic/shared_mutex/task.h
+++ atomic/shared_mutex/task.h
@@ -1,16 +1,33 @@
 #pragma once
+#include <atomic>
+#include <iostream>
+#include <thread>
 
 class SharedMutex {
 public:
     void lock() {
+        while (IsExclusive_.load() && Shared_.load() > 0){}
+        IsExclusive_.store(true);
     }
 
     void unlock() {
+        if(IsExclusive_.load())
+            IsExclusive_.store(false);
     }
 
     void lock_shared() {
+        while(IsExclusive_.load()){}
+        Shared_.fetch_add(1);
     }
 
     void unlock_shared() {
+        if(Shared_.load() > 0){
+            Shared_.fetch_add(-1);
+        }
     }
+private:
+    std::atomic<bool> IsExclusive_ = false;
+    std::atomic<int> Shared_ = 0;
+    //std::atomic<bool> isSharing;
+
 };
